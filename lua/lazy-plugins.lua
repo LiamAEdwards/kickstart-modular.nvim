c78112836408e5a0b9acc5f75a83a05dff696596
LiamAEdwards <liam.a.edwards@protonmail.com>
2025-02-06 01:32:51 +0000

    Your commit message for submodule

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,6 +12,7 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'psf/black',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -19,7 +20,6 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
-
 
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
